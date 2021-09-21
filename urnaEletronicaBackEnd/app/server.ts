2efcde557bd33d22343e75890b9e7992e55eb16f
Juan Ribeiro Silva Pereira <juan.pereira@capgemini.com>
2021-09-21 10:23:42 -0300

    Criação da função de leitura de arquivos e criação da rota de configuração.

--- urnaEletronicaBackEnd/app/server.ts
+++ urnaEletronicaBackEnd/app/server.ts
@@ -11,7 +11,33 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 const porta = 3001
-app.listen(porta,function () {
+app.listen(porta,async function () {
     console.log(`Server rodando na porta ${porta}`);
+});
 
-})
+app.get("/tipoDeVotacao",async function (request,response) {
+    let resposta = await lerArquivo("config",".csv",",","");
+    response.send(resposta);
+});
+
+
+async function lerArquivo(arquivo:string,extensao:string,separador:string,endereco?:string) {
+if (endereco == undefined) endereco = "";
+return new Promise(function (resolve,reject) {
+        fs.readFile(endereco+arquivo+extensao,"utf-8",function (err,data) {
+            if (err) {
+                reject("Erro ao ler arquivo: " + arquivo + extensao+" "+err);
+            }else{
+                let vetorInicial: Array<any> = data.split("\r\n");
+                let vetorFormatado: Array<any> = [];
+                vetorInicial.forEach(element => {
+                    vetorFormatado.push(element.split(separador));
+                });
+                resolve(vetorFormatado);
+                
+            }
+        })
+        
+    })
+    
+}
