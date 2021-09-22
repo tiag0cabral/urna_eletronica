da8fcae8bbb0035323c2215ed0ae7f4265d039ea
Juan Ribeiro Silva Pereira <juan.pereira@capgemini.com>
2021-09-22 10:44:02 -0300

    Criação da estrutura do componente login

--- urnaEletronicaFrontEnd/src/app/app-routing.module.ts
+++ urnaEletronicaFrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { UrnaEletronicaComponent } from './urnaEletronica/urnaEletronica.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"urnaEletronica",pathMatch:"full"},
-  {path:"urnaEletronica",component:UrnaEletronicaComponent }
+  {path:"urnaEletronica",component:UrnaEletronicaComponent },
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
--- urnaEletronicaFrontEnd/src/app/app.module.ts
+++ urnaEletronicaFrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UrnaEletronicaComponent } from './urnaEletronica/urnaEletronica.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      UrnaEletronicaComponent
+      UrnaEletronicaComponent,
+      LoginComponent
    ],
   imports: [
     BrowserModule,
