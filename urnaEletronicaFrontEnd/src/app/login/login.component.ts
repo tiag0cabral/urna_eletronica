3f252256f596620370315bc3bb4b8c726e804d9a
Borges <natan.borges@capgemini.com>
2021-09-23 17:57:21 -0300

    Criação do service da tela de login e manipulação dos dados de input

--- urnaEletronicaFrontEnd/src/app/login/login.component.ts
+++ urnaEletronicaFrontEnd/src/app/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoginService } from '../service/Login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit() {
+  public email: string = "";
+  public senha: string = "";
+
+  constructor(private serviceLogin: LoginService) {
+
+  }
+
+  public login(){
+    console.log("Logou");
+
+    const informacoesDeAcesso = {
+      email: this.email,
+      password: this.senha
+    }
+
+    this.serviceLogin.enviarLogin(informacoesDeAcesso).subscribe(
+      resultado => {
+        console.log(resultado);
+    },
+    error => {
+      console.error(error);
+    })
+
   }
 
 }
